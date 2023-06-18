8e94623d041b947aa72bc1c75c61d224d3fbdc61
Kacper Smietana <kabox9@gmail.com>
2023-06-18 12:10:13 +0200

    added audio and theme icons

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,12 @@ const search = document.querySelector(".search-icon")! as HTMLDivElement;
 const selectFonts = document.querySelector('.font')!;
 const body = document.querySelector('body')!;
 
+
 const themeCheckbox = document.querySelector('.theme-checkbox')! as HTMLInputElement;
+const themeIcon = document.querySelector('.theme-icon')!;
+
+const audioBox = document.querySelector('.audio')! as HTMLDivElement;
+let audio = document.querySelector('.speech')! as HTMLAudioElement;
 
 const searchedWord = document.querySelector(
 	".searched-word"
@@ -34,6 +39,7 @@ type paths = {
 			partOfSpeech: string;
 			synonyms: Array<string>;
 		}>;
+		phonetics: Array<{text: string, audio: string}>;
 	};
 };
 
@@ -60,6 +66,7 @@ const handleChanges = (data: paths) => {
 	handleTop(data);
 	handleSource(data);
 	handleDescription(data);
+	handleAudio(data);
 };
 
 const handleTop = (data: paths) => {
@@ -135,8 +142,26 @@ const handleSource = (data: paths) => {
 const toggleTheme = () => {
 	if (themeCheckbox.checked) {
 		body.classList.add('dark')
+		themeIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon theme-icon"
+		width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none"
+		stroke-linecap="round" stroke-linejoin="round">
+		<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+		<path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
+	</svg>`
 	} else {
 		body.classList.remove('dark')
+		themeIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun-high" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+		<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+		<path d="M14.828 14.828a4 4 0 1 0 -5.656 -5.656a4 4 0 0 0 5.656 5.656z"></path>
+		<path d="M6.343 17.657l-1.414 1.414"></path>
+		<path d="M6.343 6.343l-1.414 -1.414"></path>
+		<path d="M17.657 6.343l1.414 -1.414"></path>
+		<path d="M17.657 17.657l1.414 1.414"></path>
+		<path d="M4 12h-2"></path>
+		<path d="M12 4v-2"></path>
+		<path d="M20 12h2"></path>
+		<path d="M12 20v2"></path>
+	 </svg>`
 	}
 }
 
@@ -151,11 +176,34 @@ const toggleFonts = (e:Event) => {
 	}
 }
 
+const handleAudio = (data: paths) => {
+	
+	for (const phon of data[0].phonetics) {
+		if (phon.audio.length != 0) {
+			audio.src = phon.audio
+		}
+	}
+	
+}
+
+const playAudio = () => {
+	audio.play()
+}
+
+const handleEnter = (e:KeyboardEvent) => {
+	if (e.code === 'Enter') {
+		getRequest();
+	}
+}
+
 
 selectFonts.addEventListener('click', toggleFonts)
 
 themeCheckbox.addEventListener('click', toggleTheme)
 
+audioBox?.addEventListener('click', playAudio);
+
 search.addEventListener("click", getRequest);
+document.addEventListener('keydown', handleEnter);
 getRequest();
 
